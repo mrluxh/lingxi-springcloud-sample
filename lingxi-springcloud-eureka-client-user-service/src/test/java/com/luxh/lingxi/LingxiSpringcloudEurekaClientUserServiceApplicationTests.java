1a9d1ff8f80b6b74b1206ef2d557a9435b0238c2
luxiaohua <luxiaohua@jielema.com>
2019-07-10 20:05:11 +0800

    init

---
+++ lingxi-springcloud-eureka-client-user-service/src/test/java/com/luxh/lingxi/LingxiSpringcloudEurekaClientUserServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.luxh.lingxi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LingxiSpringcloudEurekaClientUserServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ lingxi-springcloud-registry-eureka-server/src/test/java/com/luxh/lingxi/registry/eureka/LingxiSpringcloudRegistryEurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.luxh.lingxi.registry.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LingxiSpringcloudRegistryEurekaServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
